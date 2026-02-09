d9b4d7a2a0560e7b6b08c08ac6dfdb34bb9b35d0
root <root@srv1307270.hstgr.cloud>
2026-02-09 23:11:03 +0000

    chore: bump build 20260210-0811KST

--- public/prebim/app.js
+++ public/prebim/app.js
@@ -3,7 +3,7 @@
  */
 
 const STORAGE_KEY = 'prebim.projects.v1';
-const BUILD = '20260210-0808KST';
+const BUILD = '20260210-0811KST';
 
 // lazy-loaded deps
 let __three = null;
@@ -20,8 +20,8 @@ async function loadDeps(){
     import('https://esm.sh/three@0.160.0/examples/jsm/controls/OrbitControls.js'),
     import('https://esm.sh/three@0.160.0/examples/jsm/utils/BufferGeometryUtils.js'),
     import('https://esm.sh/three-bvh-csg@0.0.17?deps=three@0.160.0'),
-    import('/prebim/engine.js?v=20260210-0808KST'),
-    import('/prebim/app_profiles.js?v=20260210-0808KST'),
+    import('/prebim/engine.js?v=20260210-0811KST'),
+    import('/prebim/app_profiles.js?v=20260210-0811KST'),
   ]);
   __three = threeMod;
   __OrbitControls = controlsMod.OrbitControls;
